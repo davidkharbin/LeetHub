470f8d96078aa3e87450a08e1fe11815c5ee6e29
David Kristofer Härbin <61034363+davidkharbin@users.noreply.github.com>
2021-09-24 15:56:44 -0600

    Time: 88 ms (57.47%), Space: 40.7 MB (32.13%) - LeetHub

--- implement-strstr/implement-strstr.js
+++ implement-strstr/implement-strstr.js
@@ -4,16 +4,18 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+  if ( needle.length === 0 ) return 0;
+  if ( haystack.length < needle.length ) return -1;
   let notFound = -1;
 
 	let start = 0;
 	while ( start<= (haystack.length - needle.length) ){
 		let end = start + needle.length;
-
-		if ( haystack.substring(start, end) === needle ){
-			return start;
-		} 
-    
+    if ( haystack[start] === needle[0] ){
+      if ( haystack.substring(start, end) === needle ){
+			  return start;
+		  }
+    }
     start++;
 	}
 	return notFound;
