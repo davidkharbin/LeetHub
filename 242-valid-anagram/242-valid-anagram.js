810eefbc43821bc24d1d5b7894bc047e17829e30
David Kristofer Härbin <61034363+davidkharbin@users.noreply.github.com>
2022-06-16 10:12:37 -0600

    Time: 147 ms (20.58%), Space: 48.1 MB (20.22%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,23 +4,8 @@
  * @return {boolean}
  */
 const isAnagram = (s, t) => {
-    if (s.length !== t.length) return false;
-    let hashS = {};
-    let hashT = {};
+    let a = s.split('').sort().join('')   
+    let b = t.split('').sort().join('')   
     
-    for (let i = 0; i < s.length; i++){
-        let sLetter = s[i];
-        let tLetter = t[i];
-        
-        hashS[sLetter] = hashS[sLetter] + 1 || 1;
-        hashT[tLetter] = hashT[tLetter] + 1 || 1;
-    } 
-    
-    for (let letter of s){
-        if (hashS[letter] !== hashT[letter]){
-            return false;
-        }
-    }
-    
-    return true;
-};
+    return a === b
+}
