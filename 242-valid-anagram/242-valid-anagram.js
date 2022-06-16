a8fade7aaaae6bb3218f0dfdfc5f09a4b973bf02
David Kristofer Härbin <61034363+davidkharbin@users.noreply.github.com>
2022-06-16 10:36:50 -0600

    Time: 120 ms (44.80%), Space: 48.2 MB (18.20%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,6 +4,8 @@
  * @return {boolean}
  */
 const isAnagram = (s, t) => {
+    if (s.length !== t.length) return false
+    
     let a = s.split('').sort().join('')   
     let b = t.split('').sort().join('')   
     
