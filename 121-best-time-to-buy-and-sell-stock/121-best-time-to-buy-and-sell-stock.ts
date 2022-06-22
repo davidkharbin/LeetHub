2c014b94b9d5494004afb0e2a84ad8633891630d
David Kristofer Härbin <61034363+davidkharbin@users.noreply.github.com>
2022-06-21 21:06:40 -0600

    Time: 121 ms (61.92%), Space: 52.3 MB (25.87%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.ts
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.ts
@@ -2,10 +2,10 @@ function maxProfit(prices: number[]): number {
     let lowestPrice: number = Infinity
     let maxProfit: number = 0
     
-    for ( let todaysPrice of prices ){
+    for (let todaysPrice of prices){
         let todaysProfit: number = todaysPrice - lowestPrice
         
-        if ( todaysPrice < lowestPrice ){
+        if (todaysPrice < lowestPrice){
             lowestPrice = todaysPrice
         }
         
