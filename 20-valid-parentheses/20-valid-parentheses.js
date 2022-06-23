f79778db837219f9b21af9be8afab22cbb0a5dc4
David Kristofer Härbin <61034363+davidkharbin@users.noreply.github.com>
2022-06-23 09:19:34 -0600

    Time: 78 ms (65.54%), Space: 41.5 MB (98.08%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,28 +3,54 @@
  * @return {boolean}
  */
 const isValid = function(s) {
-    let openers = { '{': 1, '[': 2, '(': 3 }
-    let closers = { '}': 1, ']': 2, ')': 3 }
+    if (s.length % 2 == 1) return false
     let stack = []
-    
-    if ( s.length % 2 == 1 || s[0] in closers ){
-        return false  
-    } 
+    let map = new Map([
+        ['{', '}'],
+        ['(', ')'],
+        ['[', ']']
+    ])
     
     for ( let currentChar of s ){
-        if ( currentChar in openers ){
+        if ( map.get(currentChar) ){
             stack.push(currentChar)
-            continue
-        } else if ( stack.length == 0 )
-            return false
-        
-        
-        
-        if ( openers[stack.at(-1)] == closers[currentChar] )
-            stack.pop()
-        else stack.push(currentChar)
-        
+        } else {
+            if ( stack.length == 0 ){
+                return false
+            }
+            
+            let last = stack.pop()
+            if (map.get(last) != currentChar){
+                return false;
+            }
+        }
     }
     
     return stack.length == 0
 }
+
+// const isValid = function(s) {
+//     let openers = { '{': 1, '[': 2, '(': 3 }
+//     let closers = { '}': 1, ']': 2, ')': 3 }
+//     let stack = []
+    
+//     if ( s.length % 2 == 1 || s[0] in closers ){
+//         return false  
+//     } 
+    
+//     for ( let currentChar of s ){
+//         if ( currentChar in openers ){
+//             stack.push(currentChar)
+//             continue
+//         } else if ( stack.length == 0 )
+//             return false
+        
+        
+        
+//         if ( openers[stack.at(-1)] == closers[currentChar] )
+//             stack.pop()
+//         else stack.push(currentChar)
+        
+//     }
+    
+//     return stack.length == 0
