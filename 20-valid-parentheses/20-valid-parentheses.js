e0d9b76cd767909cc5ae4f6df095af26c28088fe
David Kristofer Härbin <61034363+davidkharbin@users.noreply.github.com>
2022-06-23 09:31:44 -0600

    Time: 56 ms (98.54%), Space: 42.5 MB (27.66%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 const isValid = function(s) {
-    if (s.length % 2 == 1) return false
+    
     let stack = []
     let map = new Map([
         ['{', '}'],
@@ -14,43 +14,14 @@ const isValid = function(s) {
     for ( let currentChar of s ){
         if ( map.get(currentChar) ){
             stack.push(currentChar)
+        } else if ( stack.length == 0 ){
+            return false
         } else {
-            if ( stack.length == 0 ){
-                return false
-            }
-            
             let last = stack.pop()
             if (map.get(last) != currentChar){
                 return false;
             }
         }
     }
-    
     return stack.length == 0
 }
-
-// const isValid = function(s) {
-//     let openers = { '{': 1, '[': 2, '(': 3 }
-//     let closers = { '}': 1, ']': 2, ')': 3 }
-//     let stack = []
-    
-//     if ( s.length % 2 == 1 || s[0] in closers ){
-//         return false  
-//     } 
-    
-//     for ( let currentChar of s ){
-//         if ( currentChar in openers ){
-//             stack.push(currentChar)
-//             continue
-//         } else if ( stack.length == 0 )
-//             return false
-        
-        
-        
-//         if ( openers[stack.at(-1)] == closers[currentChar] )
-//             stack.pop()
-//         else stack.push(currentChar)
-        
-//     }
-    
-//     return stack.length == 0
