d38533af33d56c87998507986e3bc385a7b08608
David Kristofer Härbin <61034363+davidkharbin@users.noreply.github.com>
2022-06-24 12:10:19 -0600

    Time: 91 ms (50.13%), Space: 45.4 MB (14.98%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = function(nums, target) {
+    let L = 0
+    let R = nums.length - 1
+    if (nums[R] == target) return R
+    if (nums[L] == target) return L
+    while ( L <= R ){
+        
+        let mid = Math.floor( (L + R) / 2 )
+        if ( nums[mid] > target ){
+            R = mid - 1
+        } else if ( nums[mid] < target ){
+            L = mid + 1
+        } else {
+            return mid
+        }
+    }
+    
+    return -1
+}
