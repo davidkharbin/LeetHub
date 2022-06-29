603ce1108e06b539ca859b7797a2e72160cbcce2
David Kristofer Härbin <61034363+davidkharbin@users.noreply.github.com>
2022-06-28 20:59:37 -0600

    Time: 100 ms (55.11%), Space: 44.5 MB (94.07%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prevNode = null
+    let current = head
+    
+    while (current){
+        let nextNode = current.next
+        current.next = prevNode
+        prevNode = current
+        current = nextNode
+    }
+    
+    return prevNode
+}
