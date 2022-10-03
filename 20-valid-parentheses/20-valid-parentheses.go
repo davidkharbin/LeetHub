80fd9f29faa50b43100cb6294404d8cefe6324f1
David Härbin <61034363+davidkharbin@users.noreply.github.com>
2022-10-03 10:22:56 -0600

    Time: 3 ms (36.08%), Space: 2 MB (42.77%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.go
+++ 20-valid-parentheses/20-valid-parentheses.go
@@ -1,31 +1,70 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-const isValid = function(str) {
+var closedOpenPair = map[rune]rune{
+        ')': '(', 
+        ']': '[', 
+        '}': '{',
+    }
+
+type stack struct {
+    data []rune
+}
+
+func (s *stack) Add(b rune) {
+    s.data = append(s.data, b)
+}
+
+func (s *stack) Pop() rune {
+    if len(s.data) == 0 {
+        return 0
+    }
+    val := s.data[len(s.data) - 1]
+    s.data = s.data[:len(s.data) - 1]
+    return val
+}
+
+func assertLastOpening(lastOpening rune, currentClosing rune) bool {
+    if lastOpening == 0 {
+        return false
+    }
     
-    let stack = []
-    let map = new Map([
-        ['{', '}'],
-        ['(', ')'],
-        ['[', ']']
-    ])
+    // get the opening bracket we would want given the current closing one
+    // if want does not match the last one we saw, return false
+    if want, _ := closedOpenPair[currentClosing]; want != lastOpening {
+        return false
+    }
     
-    for ( let char of str ){
-        if ( map.has(char) ){
-            stack.push(char)
-            
-        } else if ( stack.length == 0 ){
-            return false
-        
-        } else {
-            let last = stack.pop()
-            if ( map.get(last) !== char )
-               return false
+    return true
+}
+
+func isValid(s string) bool {
+    if len(s) < 2 {
+        return false
+    }
+    
+    
+    seen := &stack{}
+
+    for _, bracket  := range s {        
+        switch bracket {
+            // if opening just push onto stack
+            case '(', '[', '{':
+                seen.Add(bracket)
+            // else check if the last opening
+            // bracket matches the current closing one
+            // if so, remove the last open one from the
+            // stack and continue
+            // else return false due to missmatch
+            default:
+                lastOpening := seen.Pop()
+                if !assertLastOpening(lastOpening, bracket) {
+                    return false
+                }
         }
     }
     
-    return stack.length === 0
+    // stack must be empty
+    // else we've seen more opening than closing brackets
+    if len(seen.data) != 0 {
+        return false
+    }
+    return true
 }
-
-// testing LeetHub extension(not working with new UI?)
