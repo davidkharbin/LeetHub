649d1e1941f377301e42554b0a590d7bc748eefe
David Härbin <61034363+davidkharbin@users.noreply.github.com>
2023-07-25 09:27:57 -0600

    Time: 60 ms (79.13%), Space: 42.7 MB (99.67%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
@@ -10,16 +10,12 @@ function searchMatrix(matrix: number[][], target: number): boolean {
             let M: number = Math.floor((L + R) / 2);
             let current: number = matrix[i][M];
             
-            if (target === matrix[i][L]) return true;
-            if (target === matrix[i][R]) return true;
             if (target === current) return true;  
             
             if (target < current){
                 R = M - 1;
-                L++;
             } else if (target > current){
                 L = M + 1;
-                R--;
             }
         }
     }
